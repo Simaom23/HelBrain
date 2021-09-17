dc10b8f7e5c3ca459ea710fc4249c2627854e21e
Simaom23 <simaocm23@gmail.com>
2021-09-17 18:30:12 +0100

    Added templates and started app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import os
 import sqlite3
+
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
+
+from functools import wraps
 
 # Configure application
 app = Flask(__name__)
@@ -29,4 +32,29 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure SQLite database
-db = sqlite3.connect("sqlite:///app.db")
+#db = sqlite3.connect("sqlite:///app.db")
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+
+@app.route("/")
+@login_required
+def index():
+    return render_template("index.html")
