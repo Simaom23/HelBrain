3d4223925b6b111b8bf50e13e9dff72ca4780662
Simaom23 <simaocm23@gmail.com>
2021-11-13 12:44:57 +0000

    Fixed some errors

--- app.py
+++ app.py
@@ -38,41 +38,7 @@ db = database.cursor()
 
 @ app.route("/", methods=["GET"])
 def index():
-    return redirect("/login")
-
-
-@ app.route("/login", methods=["GET", "POST"])
-def login():
-    # Forget any user_id
-    session.clear()
-
-    # User reached route via POST (as by submitting a form via POST)
-    if request.method == "POST":
-
-        # Ensure username was submitted
-        if not request.form.get("username"):
-            return template("login.html", "must provide username", 400)
-
-        # Ensure password was submitted
-        elif not request.form.get("password"):
-            return template("login.html", "must provide password", 400)
-
-        # Query database for email
-        rows = db.execute("SELECT * FROM users WHERE username = ?",
-                          request.form.get("username"))
-
-        # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return template("/login", "invalid username and/or password", 400)
-
-        # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
-
-        # Redirect user to home page
-        return redirect("/helper-index")
-
-    else:
-        return render_template("login.html")
+    return redirect("/homepage")
 
 
 @ app.route("/register", methods=["GET", "POST"])
@@ -108,33 +74,26 @@ def helper_register():
         if len(rows) != 0:
             return template("helper-register.html", "Email already in use!", 400)
 
-        # Ensure contact was submitted
-        elif not request.form.get("contact"):
-            return template("helper-register.html", "Insert Contact!", 400)
+        # Ensure email was submitted
+        if not request.form.get("email"):
+            return template("helper-register.html", "Insert Email!", 400)
 
-        # Ensure specialty was submitted
-        elif not request.form.get("specialty"):
-            return template("helper-register.html", "Insert Specialty!", 400)
-
-        # Ensure password was submitted
-        elif not request.form.get("password"):
-            return template("helper-register.html", "Insert Password", 400)
-
-        # Ensure confirmation equals password
-        elif request.form.get("password") != request.form.get("confirmation"):
-            return template("helper-register.html", "Passwords do not match!", 400)
+        # Query database for email
+        db.execute("SELECT * FROM helper WHERE email = ?",
+                   (request.form.get("email"),))
+        specialty = db.fetchall()
 
         # Inserts new user to the database
-        db.execute("INSERT INTO users(username, hash) VALUES(?, ?)", (request.form.get(
-            "username"), generate_password_hash(request.form.get("password"))))
+        db.execute("INSERT INTO helpers(first_name, last_name, email, contact, specialty_id) VALUES(?, ?)", (request.form.get(
+            "firstname"), request.form.get("lastname"), request.form.get("email"), request.form.get("contact"), ))
 
         # Redirect user to home page
         flash("You're registred!")
-        return redirect("/helper-index")
+        return redirect("/login")
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
-        db.execute("SELECT name FROM specialty")
+        db.execute("SELECT name FROM specialtys")
         specialtys = db.fetchall()
         return render_template("helper-register.html", specialtys=specialtys)
 
@@ -150,6 +109,14 @@ def user_register():
         if not request.form.get("username"):
             return template("user-register.html", "Insert Username!", 400)
 
+        db.execute("SELECT * FROM user WHERE username = ?",
+                   (request.form.get("username"),))
+        rows = db.fetchall()
+
+        # Ensure new user doesn't exist
+        if len(rows) != 0:
+            return template("user-register.html", "Username already in use!", 400)
+
         # Ensure email was submitted
         if not request.form.get("email"):
             return template("user-register.html", "Insert Email!", 400)
@@ -164,11 +131,11 @@ def user_register():
             return template("user-register.html", "Email already in use!", 400)
 
         # Ensure password was submitted
-        elif not request.form.get("password"):
+        if not request.form.get("password"):
             return template("user-register.html", "Insert Password", 400)
 
         # Ensure confirmation equals password
-        elif request.form.get("password") != request.form.get("confirmation"):
+        if request.form.get("password") != request.form.get("confirmation"):
             return template("user-register.html", "Passwords do not match!", 400)
 
         # Inserts new user to the database
@@ -177,62 +144,79 @@ def user_register():
 
         # Redirect user to home page
         flash("You're registred!")
-        return redirect("/user-index")
+        return redirect("/login")
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("user-register.html")
 
 
-@ app.route("/helper-index", methods=["GET", "POST"])
-# @ login_required
-def helper_index():
+@ app.route("/login", methods=["GET", "POST"])
+def login():
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
 
-        # Ensure note is valid
-        if not request.form.get("note"):
-            return template("helper-index.html", "Write a valid note!", 400)
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return template("login.html", "Must provide username", 400)
 
-        # Ensure email was submitted
-        elif not request.form.get("email"):
-            return template("helper-index.html", "Insert Email!", 400)
-        db.execute()
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return template("login.html", "Must provide password", 400)
 
         # Query database for email
-        db.execute("SELECT * FROM user WHERE email = ?",
-                   (request.form.get("email"),))
-        rows = db.fetchall()
+        rows = db.execute("SELECT * FROM users WHERE username = ?",
+                          request.form.get("username"))
 
-        # Ensure email exists
-        if len(rows) != 0:
-            return template("helper-register.html", "Email doesn't exist!", 400)
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return template("/login", "Invalid username and/or password", 400)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/homepage")
 
     else:
-        db.execute("SELECT * FROM user WHERE email = ?",
-                   (request.form.get("email"),))
-        rating = db.fetchall()
-        return render_template("helper-index.html", rating=rating)
+        return render_template("login.html")
 
 
-@ app.route("/user-index", methods=["GET", "POST"])
-# @ login_required
-def user_index():
+@ app.route("/homepage", methods=["GET", "POST"])
+# @login_required
+def homepage():
     if request.method == "POST":
-        return
+        # Ensure note is valid
+        if not request.form.get("note"):
+            return template("homepage.html", "Write a valid note!", 400)
+        db.execute("SELECT * FROM user WHERE email = ?",
+                   (request.form.get("email"),))
+        return redirect("/homepage")
     else:
-        return render_template("user-index.html")
+        db.execute("SELECT * FROM notes username = ?",
+                   (request.form.get("email"),))
+        notes = db.fetchall()
+        return render_template("homepage.html", notes=notes)
 
 
 @ app.route("/specialists", methods=["GET", "POST"])
+# @login_required
 def specialists():
-    return render_template("specialists.html")
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        return
+    else:
+        return render_template("specialists.html")
 
 
 # Handle error
 def errorhandler(e):
     if not isinstance(e, HTTPException):
         e = InternalServerError()
-    return template("login.html", "Something Went Wrong", e.code)
+    return template("homepage.html", "Something Went Wrong", e.code)
 
 
 # Listen for errors
